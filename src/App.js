295e111455f1ea164e7784f64750ee218d939088
lavarwave.soeun <ase0817@lavarwave.co.kr>
2022-08-21 17:02:16 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+/* eslint-disable */
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let [title, titleSet] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [date, dateSet] = useState('2월 17일 발행')
+  let [like, likeSet] = useState(0);
+  /**
+   * 변수와 state의 차이점
+   * state는 변경되면 쓰던 html은 자동으로 재렌더링 됨
+   * -> 언제써야하는지? 변동시 자동으로 html에 반영되게 만들고 싶으면 state쓰면 됨
+   */
+
+  function addLike(){
+    console.log(1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+      
+      <button onClick={()=>{
+        let copy = [...title];
+        copy.sort();
+        titleSet(copy);
+      }}>가나다라순정렬</button>
+
+      <button className="btn" onClick={()=>{ 
+        // state가 array/object형식이면 shallow copy를 만들어서 수저해야함
+        let copy = [...title];
+        copy[0] = '여자 코트 추천';
+        titleSet(copy); 
+        }}>글수정</button>
+
+      <div className='list'>
+        <h4>{ title[0] } <span onClick={()=>{ likeSet( like + 1 ) }}>💚</span> { like } </h4>
+        <p>{ date }</p>
+      </div>
+      <div className="list">
+        <h4>{ title[1] }</h4>
+        <p>{ date }</p>
+      </div>
+      <div className="list">
+        <h4>{ title[2] }</h4>
+        <p>{ date }</p>
+      </div>
     </div>
   );
 }
