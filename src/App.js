bc1d5f0c79d9495c364de368877063bbaeb2b6de
lavarwave.soeun <ase0817@lavarwave.co.kr>
2022-09-22 13:11:27 +0900

    [2022.09.22:13:11][style][코드정리]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-/* eslint-disable */
-
 import './App.css';
 import { useState } from 'react';
 
 function App() {
-
   let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let [date] = useState('2월 17일 발행')
   let [like, setLike] = useState([0, 0, 0]);
@@ -25,40 +22,53 @@ function App() {
         가나다라순정렬
       </button>
 
-      <button onClick={()=>{ 
+      <button onClick={()=>{                              // 버튼을 클릭하면
         // state가 array/object형식이면 shallow copy를 만들어서 수저해야함
-        let copy = [...title];
-        copy[0] = '여자 코트 추천';
-        setTitle( copy ); 
+        let copy = [...title];                            // 변수 title 배열을 복사해서
+        copy[0] = '여자 코트 추천';                        // 0번째 배열의 값을 '여자 코트 추천' 으로 변경
+        setTitle( copy );                                 // setTitle을 통해 copy 담아서 선언
       }}>
         글수정
       </button>
 
       {
         title.map(function(param, i){
+          const styleCursor = {cursor: "pointer"}
           return (
             <div className="list" key={i}>
-              <h4 onClick={() => {
+              <h4 style={styleCursor} onClick={() => {
                 setModal(modal == true ? false : true);
                 setContentTitle(i)
               }}>
                 { title[i] }
-                <span onClick={()=>{ 
-                  let copy = [...like]
-                  copy[i] = copy[i] + 1
-                  setLike( copy ) 
+
+                <span 
+                  style={styleCursor}
+                  onClick={(e)=>{
+                    // 이벤트 버블링현상 막기 (span태그 클릭시 '상위 요소 태그 클릭시 모달창 뜨는 효과'막기)
+                    e.stopPropagation(); 
+
+                    let copy = [...like]
+                    copy[i] = copy[i] + 1
+                    setLike( copy ) 
                 }}>
                   💚
                 </span> 
+
                 { like[i] }
               </h4> 
+
               <p>{ date }</p>
             </div>
           )
         })
       }
 
-      <button>글제목</button>
+      {/* 
+        e = 이벤트 객체
+        e.target = 이벤트가 발생하는 곳
+      */}
+      <input type="text" onChange={(e)=> { console.log(e.target.value) }} />
 
       {
         modal == true ? <Modal contentTitle={contentTitle} setTitle={setTitle} title={title} /> : null
@@ -68,8 +78,7 @@ function App() {
 }
 
 // Modal Component
-function Modal(props, i){
-
+function Modal(props){
   return (
     <div className='modal'>
       <h4>{props.title[props.contentTitle]}</h4>
